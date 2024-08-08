a55504df4b8bae6743291a20be40099f615b12c0
Tim <93590862+TimVincii@users.noreply.github.com>
2024-08-09 02:01:19 +0300

    Improved config manager class.
    Now catches parsing exceptions thrown by the CommentedFileConfig, as well as logs any properties that are missing from the config file.

--- src/main/java/me/timvinci/crossbowenchants/config/ConfigManager.java
+++ src/main/java/me/timvinci/crossbowenchants/config/ConfigManager.java
@@ -1,7 +1,10 @@
 package me.timvinci.crossbowenchants.config;
 
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.ParsingException;
+import com.electronwill.nightconfig.core.io.ParsingMode;
 import com.electronwill.nightconfig.core.io.WritingMode;
+import me.timvinci.crossbowenchants.CrossbowEnchants;
 import me.timvinci.crossbowenchants.util.ColoredTextFormatter;
 import me.timvinci.crossbowenchants.util.Reference;
 import net.fabricmc.loader.api.FabricLoader;
@@ -12,69 +15,63 @@ import java.nio.file.Path;
 public class ConfigManager {
 
     private static final Path CONFIG_FILE_PATH = FabricLoader.getInstance().getConfigDir().resolve("crossbowenchants.toml");
-    private static CrossbowEnchantsConfig config = new CrossbowEnchantsConfig();
+    private static final CrossbowEnchantsConfig config = new CrossbowEnchantsConfig();
 
     public static void loadConfig() {
-        CommentedFileConfig fileConfig = CommentedFileConfig.builder(CONFIG_FILE_PATH)
-                .sync()
-                .autosave()
-                .preserveInsertionOrder()
-                .writingMode(WritingMode.REPLACE)
-                .build();
-
-        fileConfig.load();
-        if (!fileConfig.getFile().exists() || fileConfig.isEmpty()) {
-            fileConfig.set("Enabled", config.isEnabled());
-            fileConfig.setComment("Enabled", "A toggle for the global functionality of Crossbow Enchants.");
-
-            fileConfig.set("FlameEnabled", config.isFlameEnabled());
-            fileConfig.setComment("FlameEnabled", "A toggle for the functionality of the Flame enchantment on the crossbow.");
-
-            fileConfig.set("InfinityEnabled", config.isInfinityEnabled());
-            fileConfig.setComment("InfinityEnabled", "A toggle for the functionality of the Infinity enchantment on the crossbow.");
-
-            fileConfig.set("PowerEnabled", config.isPowerEnabled());
-            fileConfig.setComment("PowerEnabled", "A toggle for the functionality of the Power enchantment on the crossbow.");
-
-            fileConfig.set("PunchEnabled", config.isPunchEnabled());
-            fileConfig.setComment("PunchEnabled", "A toggle for the functionality of the Punch enchantment on the crossbow.");
-
-            fileConfig.set("InfinityAndMendingEnabled", config.isInfinityAndMendingEnabled());
-            fileConfig.setComment("InfinityAndMendingEnabled", "A toggle for the compatibility of Infinity and Mending on crossbows and bows.");
-
-            fileConfig.set("PiercingAndMultishotEnabled", config.isPiercingAndMultishotEnabled());
-            fileConfig.setComment("PiercingAndMultishotEnabled", "A toggle for the compatibility of Piercing and Multishot on crossbows.");
-
-            fileConfig.save();
-        } else {
-            config.setEnabled(fileConfig.getOrElse("Enabled", config.isEnabled()));
-            config.setFlameEnabled(fileConfig.getOrElse("FlameEnabled", config.isFlameEnabled()));
-            config.setInfinityEnabled(fileConfig.getOrElse("InfinityEnabled", config.isInfinityEnabled()));
-            config.setPowerEnabled(fileConfig.getOrElse("PowerEnabled", config.isPowerEnabled()));
-            config.setPunchEnabled(fileConfig.getOrElse("PunchEnabled", config.isPunchEnabled()));
-            config.setInfinityAndMendingEnabled(fileConfig.getOrElse("InfinityAndMendingEnabled", config.isInfinityAndMendingEnabled()));
-            config.setPiercingAndMultishotEnabled(fileConfig.getOrElse("PiercingAndMultishotEnabled", config.isPiercingAndMultishotEnabled()));
+        CommentedFileConfig fileConfig = buildFileConfig();
+        try {
+            fileConfig.load();
         }
-        fileConfig.close();
+        catch (ParsingException exception) {
+            String exceptionMessage = Reference.MOD_NAME + ": Config parsing failed, please validate the config properties in .\\config\\crossbowenchants.toml. Exception message: " + exception.getMessage();
+            throw new ParsingException(exceptionMessage);
+        }
+
+        if (!fileConfig.getFile().exists() || fileConfig.isEmpty()) {
+            fileConfig.close();
+            saveConfig();
+        } else {
+            config.setEnabled(getBooleanProperty(fileConfig, "Enabled"));
+            config.setFlameEnabled(getBooleanProperty(fileConfig, "FlameEnabled"));
+            config.setInfinityEnabled(getBooleanProperty(fileConfig, "InfinityEnabled"));
+            config.setPowerEnabled(getBooleanProperty(fileConfig, "PowerEnabled"));
+            config.setPunchEnabled(getBooleanProperty(fileConfig, "PunchEnabled"));
+            config.setInfinityAndMendingEnabled(getBooleanProperty(fileConfig, "InfinityAndMendingEnabled"));
+            config.setPiercingAndMultishotEnabled(getBooleanProperty(fileConfig, "PiercingAndMultishotEnabled"));
+            fileConfig.close();
+        }
     }
 
     public static void saveConfig() {
-        CommentedFileConfig fileConfig = CommentedFileConfig.builder(CONFIG_FILE_PATH)
-                .sync()
-                .autosave()
-                .preserveInsertionOrder()
-                .writingMode(WritingMode.REPLACE)
-                .build();
-
-        fileConfig.load();
+        CommentedFileConfig fileConfig = buildFileConfig();
+        try {
+            fileConfig.load();
+        }
+        catch (ParsingException exception) {
+            String exceptionMessage = Reference.MOD_NAME + ": Config parsing failed, please validate the config properties in .\\config\\crossbowenchants.toml. Exception message: " + exception.getMessage();
+            throw new ParsingException(exceptionMessage);
+        }
 
         fileConfig.set("Enabled", config.isEnabled());
+        fileConfig.setComment("Enabled", "A toggle for the global functionality of Crossbow Enchants.");
+
         fileConfig.set("FlameEnabled", config.isFlameEnabled());
+        fileConfig.setComment("FlameEnabled", "A toggle for the functionality of the Flame enchantment on the crossbow.");
+
         fileConfig.set("InfinityEnabled", config.isInfinityEnabled());
+        fileConfig.setComment("InfinityEnabled", "A toggle for the functionality of the Infinity enchantment on the crossbow.");
+
         fileConfig.set("PowerEnabled", config.isPowerEnabled());
+        fileConfig.setComment("PowerEnabled", "A toggle for the functionality of the Power enchantment on the crossbow.");
+
         fileConfig.set("PunchEnabled", config.isPunchEnabled());
+        fileConfig.setComment("PunchEnabled", "A toggle for the functionality of the Punch enchantment on the crossbow.");
+
         fileConfig.set("InfinityAndMendingEnabled", config.isInfinityAndMendingEnabled());
+        fileConfig.setComment("InfinityAndMendingEnabled", "A toggle for the compatibility of Infinity and Mending on crossbows and bows.");
+
         fileConfig.set("PiercingAndMultishotEnabled", config.isPiercingAndMultishotEnabled());
+        fileConfig.setComment("PiercingAndMultishotEnabled", "A toggle for the compatibility of Piercing and Multishot on crossbows.");
 
         fileConfig.save();
         fileConfig.close();
@@ -103,5 +100,25 @@ public class ConfigManager {
 
     public static CrossbowEnchantsConfig getConfig() {
         return config;
+    }
+
+    private static boolean getBooleanProperty(CommentedFileConfig fileConfig, String path) {
+        if (fileConfig.contains(path)) {
+            return fileConfig.get(path);
+        }
+        else {
+            CrossbowEnchants.LOGGER.error("Configuration property '{}' is missing from the config file, using default value 'true' instead.", path);
+        }
+
+        return true;
+    }
+
+    private static CommentedFileConfig buildFileConfig() {
+        return CommentedFileConfig.builder(CONFIG_FILE_PATH)
+                .sync()
+                .autosave()
+                .preserveInsertionOrder()
+                .writingMode(WritingMode.REPLACE)
+                .build();
     }
 }
